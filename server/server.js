28b5e9e9c64c231690d9f76e9cbaeacf0ac7cc40
Amit pandey <pandeyamit9340@gmail.com>
2025-07-15 17:00:42 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./db/db.js");
 
-
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 const oauthRoutes = require("./routes/oauth.route.js");
 const webhookRoutes = require("./routes/webhook.route.js");
@@ -23,9 +19,12 @@ app.get("/", (req, res) => {
     res.send("Schwab Webhook App is running");
 });
 
+app.get("/*any", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 const PORT = process.env.PORT || 3000;
 connectDB();
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
-  });
-  
+});
